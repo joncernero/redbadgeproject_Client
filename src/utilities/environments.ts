cdd5b1b77210b9fb8f6898549b4601245cd7ae21
Jonathan Cernero <cernero.jon@gmail.com>
2021-05-08 17:43:33 -0400

    initializing first deployment

--- src/utilities/environments.ts
+++ src/utilities/environments.ts
@@ -5,6 +5,9 @@ switch (window.location.hostname) {
   case 'localhost' || '127.0.0.1':
     APIURL = 'http://localhost:3000';
     break;
+  case 'jac-my-diligenceclient.herokuapp.com':
+    APIURL = 'https://kcp-jac-my-diligenceclient.herokuapp.com';
+    break;
   default:
     alert('URL Error!');
     break;
