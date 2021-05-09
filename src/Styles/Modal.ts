5440cb6a7f3d34a64a866f1be6423a8563f5d0fc
Jonathan Cernero <cernero.jon@gmail.com>
2021-05-09 12:36:27 -0400

    fixing padding on modal

--- src/Styles/Modal.ts
+++ src/Styles/Modal.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const StyledModal = styled.div`
   width: 25%;
   height: 65vh;
-  padding: 25px;
+  padding: 45px 25px;
   border-radius: 10px;
   display: flex;
   justify-content: center;
